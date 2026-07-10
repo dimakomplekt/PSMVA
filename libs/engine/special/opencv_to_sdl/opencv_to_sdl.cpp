3d81c1b8f7bfa61e5a2981631cd4de4d6a1b93b4
dimakomplekt <156188379+dimakomplekt@users.noreply.github.com>
2026-07-10 13:24:33 +0300

    new translator for check

--- libs/engine/special/opencv_to_sdl/opencv_to_sdl.cpp
+++ libs/engine/special/opencv_to_sdl/opencv_to_sdl.cpp
@@ -26,6 +26,8 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
     int height = cv_mat->rows;
 
 
+    /*
+    
     // 3. Convert color format. OpenCV uses BGR by default.
     // Translate it to the RGBA (or RGB, in dependence of sdl_texture)
 
@@ -45,9 +47,10 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
     {
         rgba_mat = *cv_mat; // 4 channels mode
     }
+    
+    */
 
-
-    // 4. Check sdl_texture - if there is no sdl_texture, or frame size have been changed - reallocate
+    // 3. Check sdl_texture - if there is no sdl_texture, or frame size have been changed - reallocate
 
     bool need_recreate = false;
 
@@ -113,7 +116,7 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
     }
 
 
-    // 5. Copy pixels from cv::Mat to SDL_Texture
+    // 4. Copy pixels from cv::Mat to SDL_Texture
 
     void* texture_pixels = nullptr;
     int texture_pitch = 0;
@@ -125,6 +128,8 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
     }
 
 
+    /*
+    
     // Block texture for write operation
     if (SDL_LockTexture(sdl_texture, nullptr, &texture_pixels, &texture_pitch))
     {
@@ -134,9 +139,9 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
             height,
     
             // Equal to basic format of OpenCV
-            SDL_PIXELFORMAT_BGR24,
-            cv_mat->data,
-            cv_mat->step,
+            SDL_PIXELFORMAT_ARGB8888
+            rgba_mat.data,
+            rgba_mat.step,
     
             // Target basic format for SDL
             SDL_PIXELFORMAT_RGBA8888,
@@ -147,6 +152,42 @@ void translate_cv_mat_to_sdl_texture(cv::Mat* cv_mat, SDL_Texture*& sdl_texture,
     
         SDL_UnlockTexture(sdl_texture);
     }
+    else 
+    {
+        SDL_Log("Can't block the texture: %s", SDL_GetError());
+    }
+    
+    */
+
+    // Block texture
+    if (SDL_LockTexture(sdl_texture, nullptr, &texture_pixels, &texture_pitch))
+    {
+        // Dynamic format check
+        SDL_PixelFormat src_format = SDL_PIXELFORMAT_UNKNOWN;
+        
+        if (cv_mat->channels() == 3) src_format = SDL_PIXELFORMAT_BGR24;          // Default OpenCV color
+        else if (cv_mat->channels() == 1) src_format = SDL_PIXELFORMAT_INDEX8;    // Specific (grayscale)
+        else if (cv_mat->channels() == 4) src_format = SDL_PIXELFORMAT_BGRA32;    // Format with alpha
+
+        
+        SDL_ConvertPixels(
+            
+            width,
+            height,
+            
+            src_format,                   // SDL format equal, which equal to current OpenCV format
+            cv_mat->data,                 // Original data
+            cv_mat->step,                 // Original step
+            
+            SDL_PIXELFORMAT_RGBA8888,     // Target format
+            texture_pixels,               // Target pixels
+            texture_pitch                 // Target pitch
+            
+        );
+    
+        SDL_UnlockTexture(sdl_texture);
+    }
+
     else 
     {
         SDL_Log("Can't block the texture: %s", SDL_GetError());
